df73842fcfcbd06e9275e837d78618f5eedb8066
Arthur Madureira <104011116+arthur-madureira@users.noreply.github.com>
2024-10-11 08:30:50 -0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'madureira.dev',
+  author: 'Arthur Madureira',
+  headerTitle: 'Arthur Madureira',
   description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  language: 'pt',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/arthur-madureira/blogtest',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
