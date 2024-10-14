989f1e1eb4977212adbffc7789e0821ebd688db5
¨Arthur <arthurrobinsonm@gmail.com>
2024-10-14 11:39:38 -0300

    metadata update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,101 +1,56 @@
+// @ts-check
 /** @type {import("pliny/config").PlinyConfig } */
+
 const siteMetadata = {
-  title: 'madureira.dev',
-  author: 'Arthur Madureira',
-  headerTitle: 'Arthur Madureira',
-  description: 'Blog para portfolio',
-  language: 'pt',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://arthur-madureira.github.io/blogtest/',
+  title: "arthur-madureira's Coding Adventure",
+  author: 'Trong Khanh',
+  fullName: 'Do Trong Khanh',
+  headerTitle: "arthur-madureira's Blog",
+  description: 'My desire to practice my skills and share my acquired knowledge fuels my endeavors.',
+  language: 'en-us',
+  theme: 'system',
+  siteUrl: 'https://blogtest',
+  analyticsURL:
+    "https://analytics.us.umami.is/share/LwbQOMMcBOK9zcwU/arthur-madureira's%20Blog%20-%20arthur-madureira's%20Coding%20Adventure",
   siteRepo: 'https://github.com/arthur-madureira/blogtest',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  // socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  // mastodon: 'https://mastodon.social/@mastodonuser',
-  // email: 'address@yoursite.com',
-  // github: 'https://github.com',
-  // x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  // facebook: 'https://facebook.com',
+  siteLogo: '/static/images/avatar.jpg',
+  image: '/static/images/avatar.jpg',
+  socialBanner: '/static/images/projects/arthur-madureira-blog.png',
+  email: 'dotrongkhanh.dev@gmail.com',
+  github: 'https://github.com/arthur-madureira',
+  // facebook: 'https://www.facebook.com/blogtest',
+  linkedin: 'https://www.linkedin.com/in/arthur-madureira',
+  // twitter: 'https://twitter.com/arthur-madureira',
   // youtube: 'https://youtube.com',
-  // linkedin: 'https://www.linkedin.com',
-  // threads: 'https://www.threads.net',
-  // instagram: 'https://www.instagram.com',
   locale: 'en-US',
-  // set to true if you want a navbar fixed to the top
-  stickyNav: false,
+  socialAccounts: {
+    github: 'arthur-madureira',
+    linkedin: 'arthur-madureira',
+    facebook: 'blogtest',
+  },
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-      // You may also need to overwrite the script if you're storing data in the US - ex:
-      // src: 'https://us.umami.is/script.js'
-      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-    },
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // If you are hosting your own Plausible.
-    //   src: '', // e.g. https://plausible.my-domain.com/js/script.js
-    // },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    umamiWebsiteId: '7b7953a7-de2e-4e30-9a29-1a4aee05c627',
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
-    // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
   comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: 'giscus',
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      mapping: 'title',
+      reactions: '1',
       metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
+      inputPosition: 'top',
     },
   },
-  search: {
-    provider: 'kbar', // kbar or algolia
-    kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
-    },
-    provider: 'algolia',
-    algoliaConfig: {
-      // The application ID provided by Algolia
-      appId: 'R2IYF7ETH7',
-      // Public API key: it is safe to commit it
-      apiKey: '599cec31baffa4868cae4e79f180729b',
-      indexName: 'docsearch',
-    },
-  },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-const { withContentlayer } = require('next-contentlayer2')
+const { withContentlayer } = require('next-contentlayer');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app us.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
+  media-src 'none';
   connect-src *;
   font-src 'self';
   frame-src giscus.app
-`
+`;
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
@@ -52,49 +52,36 @@ const securityHeaders = [
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
   },
-]
-
-const output = process.env.EXPORT ? 'export' : undefined
-const basePath = process.env.BASE_PATH || undefined
-const unoptimized = process.env.UNOPTIMIZED ? true : undefined
+];
 
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer]
+  const plugins = [withContentlayer, withBundleAnalyzer];
   return plugins.reduce((acc, next) => next(acc), {
-    output,
-    basePath,
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    transpilePackages: ['lucide-react'],
     eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
+      dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
     },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-        },
-      ],
-      unoptimized,
-    },
+    output: 'standalone',
     async headers() {
       return [
         {
           source: '/(.*)',
           headers: securityHeaders,
         },
-      ]
+      ];
     },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      })
+      });
 
-      return config
+      return config;
     },
-  })
-}
+  });
+};
